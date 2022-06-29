d3b1c8df3be29cdfcc066209d7bdb07b45879fba
Joel Bernerman <joel.bernerman@helsingborg.se>
2022-06-29 15:54:56 +0200

    ci: add removables, deploy script changes

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # Script to make a deployment
 echo Starting deployment.
+git stash
 git pull
 git status
-git checkout master
-composer update
+composer update --no-install
 git add .
 git commit -m "ci: update lockfile"
 git push origin
