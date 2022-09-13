a1b038ef44144086fd23ddad3eb9adbc61786a7c
Joel Bernerman <joel.bernerman@helsingborg.se>
2022-09-13 16:55:08 +0200

    ci: update fakerpress

---
+++ config/ad.php
@@ -0,0 +1,12 @@
+<?php 
+
+ /**
+ * Integration with ad
+ * @var string
+ */
+ define('AD_INTEGRATION_URL', 'https://intranat.helsingborg.se/ad-api/');
+ define('AD_UPDATE_NAME', true);
+ define('AD_UPDATE_EMAIL', true);
+ define('AD_SAVE_PASSWORD', true);
+ define('AD_RANDOM_PASSWORD', false);
+ define('AD_USER_DOMAIN', 'helsingborg.se');
