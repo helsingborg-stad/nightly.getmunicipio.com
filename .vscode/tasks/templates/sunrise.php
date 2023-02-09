3d0f963cb632faf1fcb1dae0879069507d2a0b30
Thor Brink <thor.brink@helsingborg.se>
2023-02-09 09:28:41 +0000

    Update sunrise.php for devcontainer

--- .vscode/tasks/templates/sunrise.php
+++ .vscode/tasks/templates/sunrise.php
@@ -8,4 +8,11 @@ if ( !defined( 'SUNRISE_LOADED' ) ) {
 }
 
 $domain = $_SERVER[ 'HTTP_HOST' ];
+$prefix = $wpdb->base_prefix;
+$optionsTable = "${prefix}options";
+
 $wpdb->query( $wpdb->prepare("UPDATE $wpdb->blogs SET domain = %s WHERE domain != %s", $domain, $domain) );
+$wpdb->query( $wpdb->prepare("UPDATE $wpdb->site SET domain = %s WHERE domain != %s", $domain, $domain) );
+$wpdb->query( $wpdb->prepare("UPDATE $wpdb->sitemeta SET meta_value = %s WHERE meta_key = 'siteurl' AND meta_value != %s", WP_SITEURL, WP_SITEURL) );
+$wpdb->query( $wpdb->prepare("UPDATE $optionsTable SET option_value = %s WHERE option_name = 'siteurl' AND option_value != %s", WP_SITEURL, WP_SITEURL) );
+$wpdb->query( $wpdb->prepare("UPDATE $optionsTable SET option_value = %s WHERE option_name = 'home' AND option_value != %s", WP_HOME, WP_HOME) );
